fac44c0593b7d49957dfa066ee4d1c7e3b024b50
svanesch <sja.v.esch@gmail.com>
2024-05-16 16:04:48 +0200

    minor updates

--- src/tustin_net_py/utils/tustinLoss.py
+++ src/tustin_net_py/utils/tustinLoss.py
@@ -3,7 +3,7 @@ import torch
 def Loss(prediction, target):
     pos_diff = torch.cos(prediction[..., 0:2] - target[..., 0:2])           # shape: (batch_size, 2)
 
-    loss = torch.mean(2*(1 - pos_diff))# + (prediction[..., 2:4] - target[..., 2:4])**2) 
+    loss = torch.mean(2*(1 - pos_diff))
 
     return loss
 
